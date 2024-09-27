95ef575cd285757cca63be2d80fce8eaed9d25bd
HowXu <howxu366@outlook.com>
2024-09-27 12:42:00 +0800

    finish: clippy

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -3,12 +3,12 @@
 // Execute `rustlings hint clippy2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    for x in option {
+    //原本是for循环 但是拆不了Result包的 应该if let
+    //while let也不能用会直接溢出
+    if let Some(x) = option {
         res += x;
     }
     println!("{}", res);
