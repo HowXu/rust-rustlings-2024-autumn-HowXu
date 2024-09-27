e393b9c17bfa0059d57279f2e490ed8c54fcd61d
HowXu <howxu366@outlook.com>
2024-09-27 11:59:58 +0800

    finish: macros

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -5,8 +5,10 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+
+#[macro_use]
+//用了宏的mod要声明
 mod macros {
     macro_rules! my_macro {
         () => {
@@ -14,6 +16,8 @@ mod macros {
         };
     }
 }
+
+
 
 fn main() {
     my_macro!();
--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,16 +3,15 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
+    //加上分号就可以了 一个分号语句是一个规则
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
 }
 
 fn main() {
