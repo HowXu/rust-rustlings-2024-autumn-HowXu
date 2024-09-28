dc11d7ddf3460e833759dd5979c877f5fbd3b50f
HowXu <howxu366@outlook.com>
2024-09-28 13:59:03 +0800

    finish: BFS

--- exercises/algorithm/algorithm5.rs
+++ exercises/algorithm/algorithm5.rs
@@ -2,9 +2,9 @@
 	bfs
 	This problem requires you to implement a basic BFS algorithm
 */
-
-//I AM NOT DONE
 use std::collections::VecDeque;
+
+// 广度优先搜索
 
 // Define a graph
 struct Graph {
@@ -21,6 +21,7 @@ impl Graph {
 
     // Add an edge to the graph
     fn add_edge(&mut self, src: usize, dest: usize) {
+        //双向图表可访问
         self.adj[src].push(dest); 
         self.adj[dest].push(src); 
     }
@@ -30,7 +31,36 @@ impl Graph {
         
 		//TODO
 
+        //BFS是要用队列的
+        //首先初始化一个返回值
         let mut visit_order = vec![];
+        //创建一个表示已经访问的节点的数组
+        let mut have_visited = vec![false;self.adj.len()];
+        let mut queue = VecDeque::new();//非用不可的队列
+
+        queue.push_back(start); //加入头
+
+        //遍历周围的节点
+        while let Some(i) = queue.pop_front() {
+
+            //从队列中访问了这个节点就加进去 这样只有进了队列的才会被push 同时下面有判断机制 队列不会有重复元素
+            visit_order.push(i);
+            //这里访问了就标记为已访问
+            have_visited[i] = true;
+            //println!("push {i}");
+
+            for index in self.adj[i].clone() {
+                //必须是未访问的才能进入have_visited
+                if !have_visited[index] {
+                    //标记已经访问
+                    have_visited[index] = true;
+                    queue.push_back(index); //下一次就从这里开始
+                    
+                }
+            }
+        }
+        //如此循环直到把队列中所有被push进去的节点访问一遍
+
         visit_order
     }
 }
