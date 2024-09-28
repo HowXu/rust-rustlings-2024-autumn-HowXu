d96f6dfc5ad4999e94cf8226a05b42b0cbe0822c
HowXu <howxu366@outlook.com>
2024-09-28 16:03:51 +0800

    test

--- exercises/algorithm/algorithm7.rs
+++ exercises/algorithm/algorithm7.rs
@@ -3,7 +3,6 @@
 	This question requires you to use a stack to achieve a bracket match
 */
 
-// I AM NOT DONE
 #[derive(Debug)]
 struct Stack<T> {
 	size: usize,
@@ -31,8 +30,10 @@ impl<T> Stack<T> {
 		self.size += 1;
 	}
 	fn pop(&mut self) -> Option<T> {
-		// TODO
-		None
+		//出栈
+		let result = self.data.remove(self.size);
+		self.size -= 1;
+		Some(result)
 	}
 	fn peek(&self) -> Option<&T> {
 		if 0 == self.size {
@@ -102,6 +103,12 @@ impl<'a, T> Iterator for IterMut<'a, T> {
 fn bracket_match(bracket: &str) -> bool
 {
 	//TODO
+	//解析是不是一串完整的括号
+
+	//首先解析字符串
+	let strings = bracket.chars();
+	//println!("{:?}",strings);
+	//match
 	true
 }
 
