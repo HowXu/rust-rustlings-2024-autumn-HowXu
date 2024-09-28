192e74a6c912c5b8d610eb6ad2b1865ce3ef9d48
HowXu <howxu366@outlook.com>
2024-09-28 23:43:02 +0800

    finish:图 and finish all

--- exercises/algorithm/algorithm10.rs
+++ exercises/algorithm/algorithm10.rs
@@ -1,21 +1,25 @@
 /*
-	graph
-	This problem requires you to implement a basic graph functio
+    graph
+    This problem requires you to implement a basic graph function
 */
-// I AM NOT DONE
+
+// 图
 
 use std::collections::{HashMap, HashSet};
 use std::fmt;
 #[derive(Debug, Clone)]
 pub struct NodeNotInGraph;
+
 impl fmt::Display for NodeNotInGraph {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "accessing a node that is not in the graph")
     }
 }
+
 pub struct UndirectedGraph {
     adjacency_table: HashMap<String, Vec<(String, i32)>>,
 }
+
 impl Graph for UndirectedGraph {
     fn new() -> UndirectedGraph {
         UndirectedGraph {
@@ -28,20 +32,49 @@ impl Graph for UndirectedGraph {
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>> {
         &self.adjacency_table
     }
+    //写无向图的节点连接的函数
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        //因为是无向图 所以要接过去接过来
+
+        let (node1, node2, val) = edge; //拆包
+
+        // 先把两个节点挂上去
+        self.add_node(node1);
+        self.add_node(node2);
+        //加entry防止重复造节点
+        self.adjacency_table_mutable()
+            .entry(node1.into())
+            .or_insert(vec![])
+            .push((node2.into(), val)); //如果该节点已经存在，则直接返回该值的可变引用 不存在则创建一个空向量再传入指向信息
+        self.adjacency_table_mutable()
+            .entry(node2.into())
+            .or_insert(vec![])
+            .push((node1.into(), val));
+        //insert是按照键值对加向量 返回值是插入向量的引用
     }
 }
+
 pub trait Graph {
     fn new() -> Self;
     fn adjacency_table_mutable(&mut self) -> &mut HashMap<String, Vec<(String, i32)>>;
     fn adjacency_table(&self) -> &HashMap<String, Vec<(String, i32)>>;
     fn add_node(&mut self, node: &str) -> bool {
+        //为表添加节点
+        //首先检测这个节点值是否存在 存在就false
+        if self.adjacency_table().contains_key(node.into()) {
+            return false;
+        } else {
+            //不存在时在adjacency_table_mutable添加这个节点并给一个没有标注连接的向量
+            self.adjacency_table_mutable().insert(node.into(), vec![]);
+            return true;
+        }
         //TODO
-		true
     }
     fn add_edge(&mut self, edge: (&str, &str, i32)) {
         //TODO
+        //这个是有向图吗
+        //这个也不用写吗
     }
     fn contains(&self, node: &str) -> bool {
         self.adjacency_table().get(node).is_some()
@@ -59,6 +92,7 @@ pub trait Graph {
         edges
     }
 }
+
 #[cfg(test)]
 mod test_undirected_graph {
     use super::Graph;
